554944fa3a267c5159e683a63f9b048d90fbb333
Meth Munindradasa <methsndr@gmail.com>
2023-03-05 17:25:03 +0530

    Initial Commit

---
+++ src/SandboxApp.cpp
@@ -0,0 +1,19 @@
+#include <ArcEngine.h>
+
+class SandboxApp : public AE::Application
+{
+public:
+	SandboxApp()
+	{
+	}
+
+	~SandboxApp()
+	{
+	}
+};
+
+AE::Application* AE::CreateApplication()
+{
+	return new SandboxApp();
+}
+```
